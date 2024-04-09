9d05abae40efda27b2ca380bd51b5f45620ebdb6
unknown <bika070705@gmail.com>
2024-04-09 03:46:45 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,161 @@
-import logo from './logo.svg';
+import '../src/js/index2';
+import '../src/css/style.css';
+import '../src/App.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="wrapper">
+    <header className="header">
+      <div className="top-header">
+        <a href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" className="header-arrow"><i className="slider-button fa-solid fa-angle-left"></i></a>
+        <h1>Back home</h1>
+      </div>
+      <h2>Top Rated movies</h2>
+    </header>
+    <main className="main">
+      <div className="container">
+        <div className='slider-wrapper'>
+          <button id='prev-slide' className='slider-button fa-solid fa-angle-left'></button>
+          <div className='image-list'>
+
+            <a href='http////' className='image-item'>
+              <div className="avatar">
+                <img src="https://ik.imagekit.io/kwakfvayp/New%20Folder/img/avatar-poster.png?updatedAt=1712514206079" alt=""/>
+                <h1>Avatar</h1>
+                <span>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                </span>
+                <h2>10 Oct 2021</h2>
+                <p>Avatar release is expected to draw large
+                  crowds due to popularity | NewsBytes</p>
+            </div>
+            </a>
+
+            <a href='http///' className='image-item'>
+              <div className="">
+                <img src="https://ik.imagekit.io/kwakfvayp/New%20Folder/img/venom-poster.png?updatedAt=1712514205260" alt=""/>
+                <h1>Venom 2 </h1>
+                <span>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                </span>
+                <h2>10 Oct 2021</h2>
+                <p>Let There Be Carnage' release delayed to September | NewsBytes</p>
+            </div>
+            </a>
+
+            <a href='http///' className='image-item'>
+            <div className="">
+              <img src="https://ik.imagekit.io/kwakfvayp/New%20Folder/img/bloodshot-poster.png?updatedAt=1712514204216" alt=""/>
+              <h1>Bloodshot</h1>
+              <span>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+              </span>
+              <h2>10 Oct 2021</h2>
+              <p>Ray Garrison, a soldier recently killed in action, is brought back to life as the superhero ...
+              </p>
+          </div>
+            </a>
+            <a href='http///' className='image-item'>
+              <div className="">
+                <img src="https://ik.imagekit.io/kwakfvayp/New%20Folder/img/theCityOfGold-poster.png?updatedAt=1712514204206" alt=""/>
+                <h1>The City of Gold</h1>
+                <span>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                </span>
+                <h2>10 Oct 2021</h2>
+                <p>English Movies: Watch English Movies 2021, Latest Hollywood Movies Online | English Films</p>
+            </div>
+            </a>
+            <a href='http////' className='image-item'>
+              <div className="avatar">
+                <img src="https://ik.imagekit.io/kwakfvayp/New%20Folder/img/avatar-poster.png?updatedAt=1712514206079" alt=""/>
+                <h1>Avatar</h1>
+                <span>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                </span>
+                <h2>10 Oct 2021</h2>
+                <p>Avatar release is expected to draw large
+                  crowds due to popularity | NewsBytes</p>
+            </div>
+            </a>
+
+            <a href='http///' className='image-item'>
+              <div className="">
+                <img src="https://ik.imagekit.io/kwakfvayp/New%20Folder/img/venom-poster.png?updatedAt=1712514205260" alt=""/>
+                <h1>Venom 2 </h1>
+                <span>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                </span>
+                <h2>10 Oct 2021</h2>
+                <p>Let There Be Carnage' release delayed to September | NewsBytes</p>
+            </div>
+            </a>
+
+            <a href='http///' className='image-item'>
+            <div className="">
+              <img src="https://ik.imagekit.io/kwakfvayp/New%20Folder/img/bloodshot-poster.png?updatedAt=1712514204216" alt=""/>
+              <h1>Bloodshot</h1>
+              <span>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+              </span>
+              <h2>10 Oct 2021</h2>
+              <p>Ray Garrison, a soldier recently killed in action, is brought back to life as the superhero ...
+              </p>
+          </div>
+            </a>
+            <a href='http///' className='image-item'>
+              <div className="">
+                <img src="https://ik.imagekit.io/kwakfvayp/New%20Folder/img/theCityOfGold-poster.png?updatedAt=1712514204206" alt=""/>
+                <h1>The City of Gold</h1>
+                <span>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                </span>
+                <h2>10 Oct 2021</h2>
+                <p>English Movies: Watch English Movies 2021, Latest Hollywood Movies Online | English Films</p>
+            </div>
+            </a>
+          </div>
+          <button id='next-slide' className='slider-button fa-solid fa-angle-right'></button>
+        </div>
+      </div>
+    </main>
+  </div>
+
+);
 }
 
 export default App;
+
